f0b4e65471b69196802266d061d4b7bd44543a72
Ben Okello Mwaka <mwakaben@gmail.com>
2020-11-09 12:03:18 +0300

    My Firs Commit to the Android Cohort

---
+++ Code/src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main (String args[]){
+        // to print an output
+        System.out.println("Hello Uganda Welcome to the Android Accelerator program");
+        System.out.println("HMy Name is Ben");
+        System.out.println("I am a Student,"+" Pleased to be here");
+    }
+}
